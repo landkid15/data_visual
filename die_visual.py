c47e933e8b49c901fc6d0caa3a1da4fdc4f3e0ff
will <duweibj@outlook.com>
2022-06-04 08:15:15 +0800

    reuslts列表解析

--- die_visual.py
+++ die_visual.py
@@ -3,14 +3,9 @@ import pygal
 # 创建一个D6
 die = Die()
 # 掷几次骰子，并将结果存储在一个列表中
-results =[]
-for roll_num in range(1000):
-    result = die.roll()
-    results.append(result)
+results =[die.roll() for x in range(1000)]
 # 分析结果
-
 frequencies = [results.count(value)for value in range(1,die.num_sides+1)]
-
 # 对结果进行可视化
 hist = pygal.Bar()
 hist.title = 'Results of rolling one D6 1000 times.'
@@ -20,5 +15,3 @@ hist.x_title = 'Result'
 hist.y_title = 'Frequency of Result'
 hist.add('D6',frequencies)
 hist.render_to_file('die_visual.svg')
-print([die.roll for value in range(1000)])
-print(frequencies)
